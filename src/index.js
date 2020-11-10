413ce93640b5945495077a046bd56a9288109951
Moin Khan <kmoin786@gmail.com>
2020-11-10 12:08:16 +0530

    add leader to the search li

--- src/index.js
+++ src/index.js
@@ -11,22 +11,26 @@ const options = (instantPlace) => ({
   body: JSON.stringify({ instantPlace }),
 });
 
+const removeAllChildNodes = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+};
+
 const ami = async (e, word) => {
-  // placeOptions.place = word;
   const resp = await fetch('https://backend.moinkhanif.dev/api/v1/weathery', options(word));
   const json = await resp.json();
   const label = e.target.parentElement;
-  label.innerHTML = '';
-  label.appendChild(e.target);
-  const ul = label.appendChild(document.createElement('ul'));
+  const ul = label.childNodes[1];
   const cities = {};
+  removeAllChildNodes(ul);
   json.places.forEach(place => {
     const citySelect = ul.appendChild(document.createElement('li'));
     citySelect.innerHTML = place.description;
     cities[place.description] = place;
     citySelect.addEventListener('click', (city) => {
       e.target.value = cities[city.target.textContent].description;
-      label.removeChild(label.childNodes[1]);
+      removeAllChildNodes(ul);
     });
   });
 };
@@ -40,15 +44,15 @@ document.querySelector('.main-nav').addEventListener('click', (e) => {
 let timeout;
 document.querySelector('.city-search-input').addEventListener('input', (e) => {
   clearTimeout(timeout);
-  timeout = setTimeout(() => {
-    const word = e.target.value;
-    if (word === '' || word.length < 2) {
-      const label = e.target.parentElement;
-      if (label.childNodes[1]) {
-        label.removeChild(label.childNodes[1]);
-      }
-    } else {
+  const label = e.target.parentElement;
+  const ul = label.childNodes[1];
+  const word = e.target.value;
+  removeAllChildNodes(ul);
+  if (word === '' || word.length >= 2) {
+    const iniLi = ul.appendChild(document.createElement('li'));
+    iniLi.classList.add('loader');
+    timeout = setTimeout(() => {
       ami(e, word);
-    }
-  }, 1000);
+    }, 1000);
+  }
 });
